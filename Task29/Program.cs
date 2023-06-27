5563b2953cbae1686c9fa2a444b9d510d807a809
Кузилин Юра <yurakuzilin@gmaio.com>
2023-06-27 19:21:56 +0300

    solve

---
+++ Task29/Program.cs
@@ -0,0 +1,2 @@
+﻿ int [] array = {2,4,2,1,1,23,4,5};
+ Console.WriteLine(string.Join(", ", array ));
---
+++ task25/Program.cs
@@ -0,0 +1,14 @@
+﻿
+     
+        Console.WriteLine("Input 2 numbers to calc");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        while(true)
+        {
+          double result = Math.Pow(num1, num2);
+          Console.WriteLine(result);
+          break;
+        }
+      
+        
